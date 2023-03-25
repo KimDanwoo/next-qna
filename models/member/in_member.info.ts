88e40898a16b1277a8729868a96bd7c79aa7743f
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-03-25 15:04:41 +0900

    230325 배포 오류 수정 -2

--- models/member/in_member.info.ts
+++ models/member/in_member.info.ts
@@ -1,7 +1,7 @@
 export interface InMemberInfo {
   uid: string
-  displayName?: string
-  email?: string
-  photoURL?: string
-  screenName?: string
+  displayName: string | null
+  email: string | null
+  photoURL: string | null
+  screenName: string | null
 }
