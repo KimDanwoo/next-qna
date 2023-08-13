5465a6c9963d0b9b6cf62fbe7f90ce988165a2a2
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2023-08-13 18:23:53 +0900

    ✨ http 수정

--- services/http.ts
+++ services/http.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const protocol = process.env.PROTOCOL || 'http'
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || '3000'
+const protocol = process.env.PROTOCOL ?? 'http'
+const host = process.env.HOST ?? 'localhost'
+const port = process.env.PORT ?? '3000'
 const baseUrl = `${protocol}://${host}:${port}`
 
 const http = axios.create({
